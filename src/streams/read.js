5fbb9d9f156246dfc3a92606abda654c86f97245
lased <61lased61@gmail.com>
2022-05-31 17:53:08 +0300

    feat: complete streams task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,13 @@
+import { createReadStream } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
 export const read = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const __filename = "files/fileToRead.txt";
+  const readStream = createReadStream(join(__dirname, __filename));
+
+  readStream.on("data", (chunk) => {
+    process.stdout.write(chunk);
+  });
 };
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,11 @@
+import { createWriteStream } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
 export const write = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const __filename = "files/fileToWrite.txt";
+  const writeStream = createWriteStream(join(__dirname, __filename));
+
+  process.stdin.pipe(writeStream);
 };
