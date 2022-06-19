2e4a520b94ae2a8c55fb7f39f5c1f7aff2023305
lased <61lased61@gmail.com>
2022-06-19 22:33:36 +0300

    fix: fix main tests

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,103 +1,117 @@
-import { Server as NativeServer, request } from "http";
+import { Server as NativeServer } from "http";
+import request from "supertest";
 import { v4 } from "uuid";
 
-import { Person } from "./modules/Person/Person.model";
+import { MESSAGES } from "./modules/Person/Person.constants";
 import { envConfig } from "./config/env";
 import app from "./main";
 
-describe("Integration tests", () => {
-  const options = {
-    method: "GET",
-    port: envConfig.PORT,
-  };
+describe("E2E tests", () => {
   let server: NativeServer;
-  let body = "";
-  let person: Person;
 
-  beforeAll((done) => {
-    server = app.listen(envConfig.PORT, done);
+  beforeAll(() => {
+    server = app.listen(envConfig.PORT);
   });
   afterAll(() => server.close());
-  beforeEach(() => (body = ""));
-  it("get person invalid", (done) => {
-    request({ ...options, path: "/apis/person" }, (res) => {
-      expect(res.statusCode).toBe(404);
-      done();
-    }).end();
-  });
-  it("get person valid", (done) => {
-    request({ ...options, path: "/api/person" }, (res) => {
-      expect(res.statusCode).toBe(200);
+  it("random actions 1", async () => {
+    let toBeBody = { username: "test", age: 2, hobbies: ["test"] };
+    let created = await request(server)
+      .post("/api/person")
+      .send({ ...toBeBody, other: null });
 
-      res.on("data", (chunk) => (body += chunk));
-      res.on("end", () => {
-        expect(JSON.parse(body)).toHaveLength(0);
-        done();
-      });
-    }).end();
-  });
-  it("get person by id invalid", (done) => {
-    request({ ...options, path: "/api/person/123" }, (res) => {
-      expect(res.statusCode).toBe(400);
-      done();
-    }).end();
-  });
-  it("get person by id valid", (done) => {
-    request({ ...options, path: `/api/person/${v4()}` }, (res) => {
-      expect(res.statusCode).toBe(404);
-      done();
-    }).end();
-  });
-  it("create person invalid", (done) => {
-    request({ ...options, method: "POST", path: `/api/person` }, (res) => {
-      expect(res.statusCode).toBe(400);
-      done();
-    }).end();
-  });
-  it("create person valid", (done) => {
-    const req = request(
-      { ...options, method: "POST", path: `/api/person` },
-      (res) => {
-        expect(res.statusCode).toBe(201);
-        res.on("data", (chunk) => (body += chunk));
-        res.on("end", () => ((person = JSON.parse(body)), done()));
-      }
-    );
-    req.write(JSON.stringify({ username: "test", age: 2, hobbies: [] }));
-    req.end();
-  });
-  it("update person valid", (done) => {
-    const newData = { username: "test 2", age: 22 };
-    const req = request(
-      { ...options, method: "PUT", path: `/api/person/${person.id}` },
-      (res) => {
-        expect(res.statusCode).toBe(200);
-        res.on("data", (chunk) => (body += chunk));
-        res.on("end", () => {
-          const updatedPerson = { ...person, ...newData };
-
-          person = JSON.parse(body);
-          expect(person).toMatchObject(updatedPerson);
-          done();
-        });
-      }
-    );
-    req.write(JSON.stringify(newData));
-    req.end();
-  });
-  it("delete person", (done) => {
-    const reqOptions = {
-      ...options,
-      method: "DELETE",
-      path: `/api/person/${v4()}`,
+    expect(created.body).toMatchObject(toBeBody);
+    expect(created.statusCode).toBe(201);
+    toBeBody = {
+      username: "test 2",
+      age: 11,
+      hobbies: ["test", "test"],
     };
+    created = await request(server).post("/api/person").send(toBeBody);
+    expect(created.body).toMatchObject(toBeBody);
+    expect(created.statusCode).toBe(201);
 
-    request(reqOptions, (res) => {
-      expect(res.statusCode).toBe(404);
-      request({ ...reqOptions, path: `/api/person/${person.id}` }, (res) => {
-        expect(res.statusCode).toBe(204);
-        done();
-      }).end();
-    }).end();
+    let list = (await request(server).get("/api/person")).body;
+    const secondPerson = list[1];
+
+    expect(list).toHaveLength(2);
+
+    let deleted = await request(server).delete(`/api/person/${list[0].id}`);
+
+    expect(deleted.body).toBe("");
+    expect(deleted.statusCode).toBe(204);
+    list = (await request(server).get("/api/person")).body;
+    expect(list).toHaveLength(1);
+    expect(list[0]).toEqual(secondPerson);
+    expect(
+      (await request(server).get(`/api/person/${secondPerson.id}`)).body
+    ).toEqual(secondPerson);
+    deleted = await request(server).delete(`/api/person/${secondPerson.id}`);
+    expect(deleted.body).toBe("");
+    expect(deleted.statusCode).toBe(204);
+  });
+  it("random actions 2", async () => {
+    let list = (await request(server).get("/api/person")).body;
+
+    expect(list).toHaveLength(0);
+
+    let body: any = { username: "test", age: 2, hobbies: ["test"] };
+    let created = await request(server).post("/api/person").send(body);
+
+    expect(created.body).toMatchObject(body);
+    expect(created.statusCode).toBe(201);
+    expect(
+      (await request(server).get(`/api/person/${created.body.id}`)).body
+    ).toEqual(created.body);
+    body = { username: "test 1", age: 21 };
+
+    let updated = await request(server)
+      .put(`/api/person/${created.body.id}`)
+      .send(body);
+
+    expect(updated.body).toMatchObject(body);
+    expect(updated.statusCode).toBe(200);
+
+    let deleted = await request(server).delete(
+      `/api/person/${created.body.id}`
+    );
+
+    expect(deleted.body).toBe("");
+    expect(deleted.statusCode).toBe(204);
+
+    let getById = await request(server).get(`/api/person/${created.body.id}`);
+
+    expect(getById.body).toMatchObject({ message: MESSAGES.NOT_FOUND });
+    expect(getById.statusCode).toBe(404);
+  });
+  it("random actions 3", async () => {
+    let getById = await request(server).get(`/api/person/${v4()}`);
+
+    expect(getById.body).toMatchObject({ message: MESSAGES.NOT_FOUND });
+    expect(getById.statusCode).toBe(404);
+
+    let body: any = { username: "test", age: 2, hobbies: [] };
+    let created = await request(server).post("/api/person").send(body);
+
+    expect(created.body).toMatchObject(body);
+    expect(created.statusCode).toBe(201);
+
+    let deleted = await request(server).delete(
+      `/api/person/${created.body.id}`
+    );
+
+    expect(deleted.body).toBe("");
+    expect(deleted.statusCode).toBe(204);
+
+    getById = await request(server).get(`/api/person/${created.body.id}`);
+    expect(getById.body).toMatchObject({ message: MESSAGES.NOT_FOUND });
+    expect(getById.statusCode).toBe(404);
+    body = { username: "test 1", age: 21 };
+
+    let updated = await request(server)
+      .put(`/api/person/${created.body.id}`)
+      .send(body);
+
+    expect(updated.body).toMatchObject({ message: MESSAGES.NOT_FOUND });
+    expect(updated.statusCode).toBe(404);
   });
 });
