2e5399a2259e84fb34cd1449f9443b1b2d2b6b40
lased <61lased61@gmail.com>
2022-06-28 15:35:31 +0300

    init: init graphql project

--- src/commands/draw/index.ts
+++
@@ -1,9 +0,0 @@
-import { drawCircle } from "./circle";
-import { drawSquare } from "./square";
-import { drawRect } from "./rect";
-
-export default {
-  draw_rectangle: drawRect,
-  draw_square: drawSquare,
-  draw_circle: drawCircle,
-};
--- src/wsServer.types.ts
+++
@@ -1,8 +0,0 @@
-export type PositionType = {
-  x: number;
-  y: number;
-};
-export type CommandResultType = Promise<{
-  position?: PositionType;
-  data?: string;
-} | void>;
